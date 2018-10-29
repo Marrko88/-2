fc241a394aac4d809c282916a29f4e0b76ce6187
Mark <markianec@mail.ru>
2018-10-29 17:03:51 +0300

    домашняя работа

---
+++ ДЗ4.py
@@ -0,0 +1,46 @@
+# Козлов Марк ДЗ занятие №1
+
+i = ""
+name = input("Ваше имя?:")
+print("Приветствую", name)
+
+print("У программы есть возрастные ограничения, вам есть 18 лет?")
+i = int(input("укажите возраст:"))
+if i > 18:
+    print("Добро пожаловать!")
+else:
+    print("Доступ запрещен!, есть ограничения по возрасту!")
+
+
+
+
+answer =""
+A = int
+B = int
+while answer != "N":
+    answer = input("вывести числа, (Y/N):" )
+    if answer == "Y":
+        A = int(input("укажите число B:"))
+        B = int(input("укажите число A:"))
+        print(A,B)
+    else:
+        pass
+
+
+
+
+
+A = int(input('Введите число A: '))
+X = int(A)
+B = int(input('Введите число B: '))
+Y = int(B)
+print('A = ', Y, 'B = ', X)
+
+
+
+
+
+
+
+
+
